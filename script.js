8ab8ef1018a5f14ca281a87f582a94062e9a7286
Tanush Sethi <tanushsethi55@gmail.com>
2022-06-03 01:23:02 +0530

    javascript added

---
+++ script.js
@@ -0,0 +1,28 @@
+var pos1 = 270;
+var pos2 = 270;
+var pos3 = 270;
+
+const rotateS = (hand) => {
+    pos1 = pos1 + 6;
+    second.style.transform = "rotate(" + pos1.toString() +"deg)";
+}
+const rotateM = (hand) => {
+    pos2 = pos2 + 6;
+    minute.style.transform = "rotate(" + pos2.toString() +"deg)";
+}
+const rotateH = (hand) => {
+    pos3 = pos3 + 6;
+    hour.style.transform = "rotate(" + pos3.toString() +"deg)";
+}
+
+var second = document.getElementById("second");
+var minute = document.getElementById("minute");
+var hour = document.getElementById("hour");
+
+second.style.transform = "rotate(" + pos1.toString() +"deg)";
+minute.style.transform = "rotate(" + pos2.toString() +"deg)";
+hour.style.transform = "rotate(" + pos3.toString() +"deg)";
+setInterval(rotateS, 1000);
+setInterval(rotateM, 1000*60);
+setInterval(rotateH, 1000*60*60);
+
